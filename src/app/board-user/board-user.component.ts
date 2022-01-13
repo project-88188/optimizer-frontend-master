0aeab1e8b7060110d1c1d53bfd27bcf36d08e88e
chaiya0899223232 <chaiya.style@gmail.com>
2022-01-13 11:23:22 +0700

    update

--- src/app/board-user/board-user.component.ts
+++ src/app/board-user/board-user.component.ts
@@ -15,22 +15,24 @@ export class BoardUserComponent implements OnInit {
 
  
  public tabChangedCount:Number =0;
- public currTabIndex:any =1;
+ public currTabIndex:any;
  
   currentUserContent:Usercontent={};
   constructor(private tokenStorage:TokenStorageService,
     private contentStorage:ContentStorageService) { }
 
   ngOnInit(): void {
+
     const read = this.tokenStorage.getUser().content;
     this.currentUserContent=JSON.parse(read);
 
     let buttGUI:GUI_MODEL =this.contentStorage.getGUI();
     if(!buttGUI.BUTT_TAB_INDEX)
     {
+      this.currTabIndex=0 
      let _buttGUI:GUI_MODEL = {
-       BUTT_TAB_INDEX:0  }
-    this.contentStorage.saveGUI(_buttGUI);    }
+       BUTT_TAB_INDEX:this.currTabIndex  }
+    this.contentStorage.saveGUI(_buttGUI);  }
     else{
       this.currTabIndex=buttGUI.BUTT_TAB_INDEX;   }
   
@@ -40,11 +42,12 @@ export class BoardUserComponent implements OnInit {
 
   tabChanged = (tabChangeEvent: MatTabChangeEvent): void => {
     let buttGUI:GUI_MODEL = this.contentStorage.getGUI();
-    buttGUI.BUTT_TAB_INDEX=tabChangeEvent.index;
+    this.currTabIndex=tabChangeEvent.index;
+    buttGUI.BUTT_TAB_INDEX=this.currTabIndex;
     this.contentStorage.saveGUI(buttGUI);
    //console.log('tabChangeEvent => ', tabChangeEvent); 
   //  console.log('index => ', tabChangeEvent.index); 
-    if(tabChangeEvent.index==0){
+    if(this.currTabIndex==0){
       this.tabChangedCount= Number(this.tabChangedCount)+1;
     }
 }
