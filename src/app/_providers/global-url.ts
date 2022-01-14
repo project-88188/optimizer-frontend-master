00e085503baeaec3eee7636d35be7e3a366b9b1f
chaiya0899223232 <chaiya.style@gmail.com>
2022-01-14 11:34:54 +0700

    update

--- src/app/_providers/global-url.ts
+++ src/app/_providers/global-url.ts
@@ -1,12 +1,12 @@
 
 //self standalone
 
-export const BASE_URL = window.location.origin;
-export const OPTIMIZER_URL = window.location.origin;
+//export const BASE_URL = window.location.origin;
+//export const OPTIMIZER_URL = window.location.origin;
 
 
 //local development computer
-//export const BASE_URL = "http://51.79.251.220";
-//export const OPTIMIZER_URL = "http://51.79.251.220";
+export const BASE_URL = "http://51.79.251.220";
+export const OPTIMIZER_URL = "http://51.79.251.220";
 
 //
