b9b9e253a1a75d36a825322bf609ab04e4880987
chaiya0899223232 <chaiya.style@gmail.com>
2022-01-15 12:25:13 +0700

    transection monitor component

--- src/app/_modules/transection/models/transection.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Transection.Model } from './transection.model';
-
-describe('Transection.Model', () => {
-  it('should create an instance', () => {
-    expect(new Transection.Model()).toBeTruthy();
-  });
-});
--- src/app/_modules/transection/models/transection.model.ts
+++
@@ -1,6 +0,0 @@
-export class Transection {
-    id?: any;
-    title?: string;
-    description?: string;
-    published?: boolean;
-}
