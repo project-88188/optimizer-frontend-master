ff097d9015a4108666e7d1bc7fe2e6a7efdd018b
chaiya srisawat <44835134+chaiya0899223232@users.noreply.github.com>
2022-01-16 22:16:15 +0700

    update

--- src/app/tables-optimizer/currency-table/currency-table.component.ts
+++ src/app/tables-optimizer/currency-table/currency-table.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { BehaviorSubject, merge, of } from 'rxjs';
+import { startWith, switchMap, catchError, map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { TablesoptimizerService } from '../tablesoptimizer.service';
+
+export interface GithubApi {
+  items: GithubIssue[];
+  total_count: number;
+}
+
+export interface GithubIssue {
+  created_at: string;
+  number: string;
+  state: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-currency-table',
   templateUrl: './currency-table.component.html',
   styleUrls: ['./currency-table.component.css']
 })
-export class CurrencyTableComponent implements OnInit {
+export class CurrencyTableComponent implements AfterViewInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
+  data: GithubIssue[] = [];
+  @ViewChild(MatSort) sort!: MatSort;
+  term$ = new BehaviorSubject<string>('');
+  resultsLength = 0;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngOnInit(): void {
+  constructor(private tableService: TablesoptimizerService { }
+
+
+
+  ngAfterViewInit() {
+    // If the user changes the sort order, reset back to the first page.
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+    merge(this.sort.sortChange, this.term$.pipe(debounceTime(1000), distinctUntilChanged()), this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap((searchTerm) => {
+          return this.tableService!.getSampleData(this.sort.active, this.sort.direction, this.paginator.pageIndex, (searchTerm && typeof searchTerm == 'string') ? searchTerm.toString() : 'repo:angular/components')
+            .pipe(catchError(() => of(null)));
+        }),
+        map(data => {
+          if (data === null) {
+            return [];
+          }
+
+          this.resultsLength = data.total_count;
+          return data.items;
+        })
+      ).subscribe(data => this.data = data);
   }
 
 }
--- src/app/tables-optimizer/tablesoptimizer.service.ts
+++ src/app/tables-optimizer/tablesoptimizer.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { GithubApi } from "./table/table.component";
+import { SortDirection } from "@angular/material/sort";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TablesoptimizerService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getSampleData(sort: string, order: SortDirection, page: number, q: string): Observable<GithubApi> {
+    return this.httpClient.get<GithubApi>(`https://api.github.com/search/issues?q=${q}&sort=${sort}&order=${order}&page=${page + 1}`);
+}
 }
