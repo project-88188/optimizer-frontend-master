9f0ba0205e503b43d4990d4ce7326774e5184955
chaiya0899223232 <chaiya.style@gmail.com>
2022-01-18 19:30:40 +0700

    update

--- src/app/_paypals/butt-deposit/butt-deposit.component.ts
+++ src/app/_paypals/butt-deposit/butt-deposit.component.ts
@@ -80,13 +80,10 @@ export class ButtDepositComponent implements OnChanges {
             layout: 'vertical'
         },
         onApprove: (data, progress) => {
-         console.log(data);
-
           this.checkoutService.OnApprove(this.checkoutitem.id,data).subscribe(()=>{
           });
         },
         onClientAuthorization: (data) => {
-         // console.log(data);
          this.checkoutService.onClientAuthorization(this.checkoutitem.id,data).subscribe(()=>{});
         },
         onCancel: (data, progress) => {
@@ -145,81 +142,4 @@ export class ButtDepositComponent implements OnChanges {
     })
   }
 
-  cancelCheckout() { 
-  
-    if(this.checkoutitem && this.checkoutitem.id)
-    {
-      this.transService.update(this.checkoutitem.id,{status:'canceled'}).subscribe(()=>{ });
-    }
-  }
-
-  rejectCheckout() {
-
-    if(this.checkoutitem && this.checkoutitem.id)
-    {
-      this.transService.update(this.checkoutitem.id,{status:'rejected'}).subscribe(()=>{ });
-    }
-  }
-
-  clientAuthorizatio(data:any) {
-
-   // this.checkoutService.checkoutreceived(0,data);
-
-    /*
-    if('COMPLETED'==data.status)
-    {
-    
-      console.log(data);
-          //
-          let  userdata=this.tokenStorage.getUser();
-          let  _content=JSON.parse(userdata.content);
-          _content.cashbalance=Number.parseFloat(_content.cashbalance)+Number.parseFloat(this.form.amount);
-          _content.deposit=Number.parseFloat(_content.deposit)+Number.parseFloat(this.form.amount);
-
-          if( _content.cashbalance &&  _content.deposit) {
-
-            this._paymentmethod= JSON.parse(_content.paymentmethod);
-
-            let _new_method:any[] =[];
-            for(let i=0; i <this._paymentmethod.length; i++) {
-              if(this._paymentmethod[i].detail!=data.payer.email_address && this._paymentmethod[i].method=='paypal') {
-                _new_method.push({method:'paypal', detail:this._paymentmethod[i].detail});  }
-            }
-            
-
-            _new_method.push({method:'paypal', detail:data.payer.email_address});
-            
-            _content.paymentmethod=JSON.stringify(_new_method);
-
-            _content.published=true;
-            
-            userdata.content=JSON.stringify(_content);
-            this.tokenStorage.saveUser(userdata);
-
-            const  resultcontent ={
-              cashbalance:_content.cashbalance,
-              deposit:_content.deposit,
-              paymentmethod:_content.paymentmethod,
-              published:_content.published,
-            }
-            
-            this.userContent.update(_content.id,resultcontent).subscribe(()=>{
-              
-              this.transService.update(this.checkoutitem.id,{
-                paymentdetail:data.payer.email_address,
-                transectiontype:'deposit',
-                transectionstatus:'completed'}).subscribe(()=>{});
-
-              this.showSuccess = true;
-          //    this.reloadPage();
-              
-            });
-
-          }
-
-          */
-    
-    }
-
 }
-
