1bb16a5476a5dda2e46d58513829409487e965be
chaiya srisawat <44835134+chaiya0899223232@users.noreply.github.com>
2022-01-20 02:27:23 +0700

    Update global-url.ts

--- src/app/_providers/global-url.ts
+++ src/app/_providers/global-url.ts
@@ -6,11 +6,11 @@
 
 
 //local development computer
-//export const BASE_URL = "http://192.168.1.7";
-//export const OPTIMIZER_URL = "http://192.168.1.7";
+export const BASE_URL = "http://192.168.1.7";
+export const OPTIMIZER_URL = "http://192.168.1.7";
 
 //
 //local development computer
-export const BASE_URL = 'https://bahts-club.uc.r.appspot.com';
-export const OPTIMIZER_URL = 'https://bahts-club.uc.r.appspot.com';
+//export const BASE_URL = 'https://bahts-club.uc.r.appspot.com';
+//export const OPTIMIZER_URL = 'https://bahts-club.uc.r.appspot.com';
 //
