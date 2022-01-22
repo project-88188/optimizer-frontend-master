480cdfd15c1f4773e492a3a8fad7bfecfe9eda3b
chaiya srisawat <44835134+chaiya0899223232@users.noreply.github.com>
2022-01-22 22:18:30 +0700

    Update global-url.ts

--- src/app/_providers/global-url.ts
+++ src/app/_providers/global-url.ts
@@ -1,13 +1,19 @@
 
 //self standalone
 
-//export const BASE_URL = window.location.origin;
-//export const OPTIMIZER_URL = window.location.origin;
+export const BASE_URL = window.location.origin;
+export const OPTIMIZER_URL = window.location.origin;
 
 
 //local development computer
-export const BASE_URL = "http://192.168.1.7";
-export const OPTIMIZER_URL = "http://192.168.1.7";
+//export const BASE_URL = "http://192.168.1.7";
+//export const OPTIMIZER_URL = "http://192.168.1.7";
+
+//
+//local development computer
+//export const BASE_URL = 'https://bahts-club.uc.r.appspot.com';
+//export const OPTIMIZER_URL = 'https://bahts-club.uc.r.appspot.com';
+//
 
 //
 //local development computer
